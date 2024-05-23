8f15157e53babbac12e8b798eaa576be8515bbc4
juliadyrdal <juliadyrdal@protonmail.com>
2024-05-23 06:28:19 +0200

    Update environment variables

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { Server } = require('socket.io');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const path = require('path');
@@ -16,24 +15,23 @@ const quizzesRouter = require('./routes/quizzes');
 
 const app = express();
 
-const clientUrl = process.env.CLIENT_URL;
+const clientUrl = process.env.CLIENT_URL || 'https://kviss.netlify.app'; // Default to your Netlify URL
 
 // Use CORS
 app.use(cors({
-    origin: clientUrl, 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    // Allow session cookie from browser to pass through
-    credentials: true, 
-  }));
+  origin: clientUrl,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow session cookie from browser to pass through
+}));
 
 // Set up mongoose connection
 const mongoDb = process.env.MONGO_URI;
-mongoose.connect(mongoDb);
+mongoose.connect(mongoDb, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "mongo connection error"));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY, 
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 app.use(logger('dev'));
